e2d0f16015b8350443ddacb1d1eab0907fe928b5
guochenshen <guochenshen@gmail.com>
2016-02-02 12:26:44 -0500

    added guochenshen's sensor lab code

--- assignments/sensor_lab/sensor_lab.ino
+++ assignments/sensor_lab/sensor_lab.ino
@@ -7,3 +7,25 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+/*
+   Sensor Lab
+   Guochen Shen
+   Ambient Light Sensor
+   TEMT6000
+*/
+/*
+void setup() {
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  float voltage = analogRead(A0) * 5.0 / 1024.0;
+  float amps = voltage / 10000.0;
+  float lux = amps * 1000000.0 * 2.0;
+  Serial.print("Lux: ");
+  Serial.println(lux);
+  delay(100);
+}
+*/
